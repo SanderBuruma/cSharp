6bd4e2b016f0fc7cc399878ed0c21551e6b5be39
Sander Buruma <sanderburuma@gmail.com>
2019-05-24 00:42:59 +0200

    now saves snake brains and loads them for demonstrations

--- SnakeGame/MainWindow.xaml.cs
+++ SnakeGame/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Timers;
 using System.Windows;
@@ -23,10 +26,14 @@ namespace SnakeGame
         private Brain SnakeBrain { get; set; }
         private Timer MyTimer { get; set; }
         private double ScoreTreshold { get; set; } = 2e3;
-        private readonly int TimerInterval = 1;
+        private int TimerInterval { get; set; } = 75;
         private readonly int RectWidth = 20;
         private readonly int RectSpacing = 2;
         private readonly Random rng = new Random();
+        private readonly BinaryFormatter Formatter = new BinaryFormatter();
+        private int savedFilesCount = 0;
+        private int savedFilesMax = 10;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -37,9 +44,8 @@ namespace SnakeGame
         /*
          * This mode should generate new SnakeBrain####.dat files which have scored above a certain treshold
          */
-        private void ModeGenerateNewBrains()
+        private void DrawFieldRectangles()
         {
-            Board1 = new Board(BoardWH);
             FieldRectangles = new Rectangle[BoardWH * BoardWH];
             for (int i = 0; i < BoardWH; i++)
                 for (int j = 0; j < BoardWH; j++)
@@ -58,12 +64,96 @@ namespace SnakeGame
                     MainGrid.Children.Add(FieldRectangles[i + j * BoardWH]);
                 }
 
+        }
+        private void ModeGenerateNewBrains()
+        {
+            NewBoard();
+            DrawFieldRectangles();
+            NewBrain();
+            while (savedFilesCount < savedFilesMax)
+            {
+                RedrawField();
+                bool gameOver = true;
+                for (int i = 0; i < 1000; i++)
+                {
+                    if (Board1.Progress1Tick())
+                    {
+                        RunBrain();
+                        gameOver = false;
+                    }
+                    else
+                    {
+                        gameOver = true;
+                        break;
+                    }
+                }
+                if (!gameOver)
+                    continue;
+
+                Board1.GameOver = true;
+                //gameover
+                double score = CalculateScore();
+
+                if (score > ScoreTreshold)
+                {
+                    RedrawField();
+
+                    try
+                    {
+                        string filename = "SnakeBrainFile" + Math.Floor(score).ToString() + "-" + DateTime.Now.Ticks.ToString() + ".dat";
+                        FileStream SnakeBrainFile =
+                            new FileStream(filename, FileMode.OpenOrCreate, FileAccess.Write);
+                        Formatter.Serialize(SnakeBrainFile, SnakeBrain);
+                        SnakeBrainFile.Close();
+                        savedFilesCount++;
+                        //MessageBox.Show("Saved snakebrain as " + filename);
+                    }
+                    catch (Exception err)
+                    {
+                        MessageBox.Show("Error saving snake brain.");
+                        MessageBox.Show(err.Message);
+                    }
+
+                    //MessageBox.Show("You crashed into your own tail and died or you ran out of time, your final score was " + string.Format("{0:N2}", score) + "\n\nGrow more quickly and grow larger to gain a larger score");
+                }
+
+                NewBoard();
+                NewBrain();
+            }
+            MessageBox.Show("End of program, " + savedFilesCount.ToString() + " brain files generated.\nReview the executables' folder to see the brain files");
+        }
+        /*
+         * this mode is intended to demonstrate an AI (aka. snake brain) from a file visibly in human observable time frames
+         */
+        private void ModeDemoAi()
+        {
+            NewBoard();
+            DrawFieldRectangles();
+
             MyTimer = new Timer();
-            MyTimer.Elapsed += new ElapsedEventHandler(ProgressEvent);
+            MyTimer.Elapsed += new ElapsedEventHandler(DemoAIEvent);
             MyTimer.Interval = TimerInterval;
             MyTimer.Enabled = true;
-
-            NewBrain();
+        }
+        private void DemoAIEvent(object source, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                DemoAICycle();
+            });
+        }
+        private void DemoAICycle()
+        {
+            RedrawField();
+            if (Board1.Progress1Tick())
+                RunBrain();
+            else
+            {//gameOver
+                MyTimer.Enabled = false;
+                MessageBox.Show("This bot scored " + string.Format("{0:N2}", CalculateScore()) + " points!\nIt crashed or it ran out of time.");
+                MyTimer.Enabled = true;
+                NewBoard();
+            }
         }
         private void RedrawField()
         {
@@ -73,43 +163,6 @@ namespace SnakeGame
                     int t = i + j * BoardWH;
                     FieldRectangles[t].Fill = ReturnFieldBrush(Board1.Fields[t]);
                 }
-        }
-        private void TimerEvent()
-        {
-            if (Board1.GameOver)
-                return;
-
-            RedrawField();
-            bool gameOver = true;
-            for (int i = 0; i < 1000; i++)
-            {
-                if (Board1.Progress1Tick())
-                {
-                    RunBrain();
-                    gameOver = false;
-                }
-                else
-                {
-                    gameOver = true;
-                    break;
-                }
-            }
-            if (!gameOver)
-                return;
-
-            Board1.GameOver = true;
-            //gameover
-            double score = Board1.Score;
-            score += Math.Pow(Board1.TailLength, 2)/Board1.Tick*1e3;
-
-            if (score > ScoreTreshold)
-            {
-                RedrawField();
-                MessageBox.Show("You crashed into your own tail and died or you ran out of time, your final score was " + string.Format("{0:N2}", score) + "\n\nGrow more quickly and grow larger to gain a larger score");
-            }
-
-            Board1 = new Board(BoardWH);
-            NewBrain();
         }
         private void RunBrain()
         {
@@ -172,6 +225,10 @@ namespace SnakeGame
         {
             SnakeBrain = new Brain(11, 3, 12, 4);
         }
+        private void NewBoard()
+        {
+            Board1 = new Board(BoardWH);
+        }
         private Brush ReturnFieldBrush(Board.Field field)
         {
 
@@ -195,12 +252,6 @@ namespace SnakeGame
             else
                 MyTimer.Enabled = true;
         }
-        private void ProgressEvent(object source, ElapsedEventArgs e) {
-            Dispatcher.Invoke(() =>
-            {
-                TimerEvent();
-            });
-        }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
@@ -217,6 +268,10 @@ namespace SnakeGame
                     break;
             }
         }
+        private double CalculateScore()
+        {
+            return Math.Pow(Board1.TailLength, 2) / Board1.Tick * 1e3;
+        }
 
         private void ModeNewBrainsBtn_Click(object sender, RoutedEventArgs e)
         {
@@ -225,13 +280,67 @@ namespace SnakeGame
                 MessageBox.Show("Invalid treshold value");
                 return;
             }
+            ScoreTreshold = dbl;
+            if (!int.TryParse(ModeNewBrainsTresholdBox.Text, out int fileMax))
+            {
+                MessageBox.Show("Invalid desired brains value, input an integer");
+                return;
+            }
+            savedFilesMax = fileMax;
 
             int len = MainGrid.Children.Count;
             for (int i = 0; i < len; i++)
                 MainGrid.Children.RemoveAt(0);
 
-            ScoreTreshold = dbl;
             ModeGenerateNewBrains();
+        }
+
+        private void ModeDemoAIButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(ModeDemoAIInterval.Text, out int interval))
+            {
+                MessageBox.Show("Invalid interval value, please input an integer");
+                return;
+            }
+            TimerInterval = interval;
+            OpenFileDialog dlg = new OpenFileDialog
+            {
+                DefaultExt = ".dat",
+                Filter = "Data Files (*.dat)|*.dat"
+            };
+
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result != true)
+            {
+                MessageBox.Show("Couldn't recover file path");
+                return;
+            }
+
+
+            if (!File.Exists(dlg.FileName))
+            {
+                MessageBox.Show("Couldn't find that file");
+                return;
+            }
+
+            try
+            {
+                FileStream SnakeBrainInstanceFromFile = new FileStream(dlg.FileName, FileMode.Open, FileAccess.Read);
+                SnakeBrain = (Brain)Formatter.Deserialize(SnakeBrainInstanceFromFile);
+                SnakeBrainInstanceFromFile.Close();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Could not load selected snake brain file for this demo");
+                MessageBox.Show(err.Message);
+                return;
+            }
+
+            int len = MainGrid.Children.Count;
+            for (int i = 0; i < len; i++)
+                MainGrid.Children.RemoveAt(0);
+
+            ModeDemoAi();
         }
     }
 }

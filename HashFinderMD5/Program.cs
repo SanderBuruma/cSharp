86c02bc118ea10d42925435a894818bbe94ced1b
Sander Buruma <sanderburuma@gmail.com>
2019-10-29 23:14:25 +0100

    Made a little beginner's (aka, inefficient) MD5 brute forcer

---
+++ HashFinderMD5/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+
+namespace HashFinderMD5
+{
+    class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("Input a password to guess: ");
+                string hash = CalculateMD5Hash(Console.ReadLine());
+                Console.WriteLine("The hash is " + hash);
+                Clipboard.SetText(hash);
+                string recoveredPassword = "";
+                List<char> newPw;
+                for (int j = 0; j < 10; j++)
+                {
+                    BigInteger powerOf = 256;
+                    for (int i = 0; i < j; i++)
+                    {
+                        powerOf *= 256;
+                    }
+
+                    for (BigInteger i = 0; i < powerOf; i++)
+                    {
+                        newPw = new List<char> { };
+                        BigInteger copyOfI = i;
+                        string preHashPw = "";
+                        for (int k = 0; k <= j; k++)
+                        {
+                            preHashPw += ((char)BigInteger.ModPow(copyOfI,1,256));
+                            copyOfI /= 256;
+                        }
+                        var testHash = CalculateMD5Hash(preHashPw);
+                        if (testHash == hash)
+                        {
+                            recoveredPassword = preHashPw;
+                            goto end;
+                        }
+                    }
+                }
+                end:;
+                Console.WriteLine();
+                Console.WriteLine("The Password is: " + recoveredPassword);
+                Console.ReadKey();
+            }
+        }
+        static string CalculateMD5Hash(string input)
+        {
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
--- RandomBigPrime/Program.cs
+++ RandomBigPrime/Program.cs
@@ -16,52 +16,94 @@ namespace RandomBigPrime
         static void Main()
         {
             var primes = new List<int>() { 2 };
-            for (int i = 3; i < 100000; i+=2)
+            for (int i = 3; i < 2000000; i += 2)
             {
                 foreach (var prime in primes)
                 {
                     if (prime * prime > i) break;
-                    if (i % prime == 0) goto goto2;
+                    if (i % prime == 0) goto goto1;
                 }
                 primes.Add(i);
-                goto2:;
+                goto1:;
             }
+            long primesCount = 0;
 
+            BigInteger p;
+            var rng = new RNGCryptoServiceProvider();
             Console.Write("Input the number of bytes the prime number needs to have: ");
             int primeByteSize = int.Parse(Console.ReadLine());
-            var rng = new RNGCryptoServiceProvider();
             byte[] bytes = new byte[primeByteSize];
-            BigInteger p;
 
+            BigInteger baseP = 2;
+            for (int i = 2; i/8 < primeByteSize; i++)
+            {
+                baseP *= 2;
+            }
             var sw = new Stopwatch();
             sw.Start();
-            int correct = 0;
-            int mistakes = 1;
-
             while (true)
             {
-                notPrime:;
-                sw.Restart();
+                goto2:;
                 rng.GetBytes(bytes);
-                p = new BigInteger(bytes) - 1;
-                if (p < 3) goto notPrime;
-                foreach (var prime in primes)
+                p = baseP + BigInteger.Abs(new BigInteger(bytes) - 1);
+                foreach (var item in primes)
                 {
-                    if (p % prime == 0) goto notPrime;
+                    if (p % item == 0) goto goto2;
                 }
-                if (BigInteger.ModPow(2, p - 1, p) == 1)
+                if (CustomModExp(2, p - 1, p) == 1)
                 {
-                    mistakes++;
-                    goto notPrime;
+                    primesCount++;
+                    Console.WriteLine(p.ToString("N0") + "\n" + (((double)sw.ElapsedMilliseconds)/primesCount).ToString("N2"));
                 }
-                correct++;
-
-                double errorRate = mistakes;
-                errorRate /= (double)(mistakes + correct);
-                errorRate = Math.Round(errorRate * 100, 5);
-                Console.WriteLine(correct.ToString("N0") + " correct guesses, " + mistakes.ToString("N0") + " mistakes. " + errorRate.ToString("N2") + "% error rate. " + primeByteSize.ToString("N0") + " bytesize.");
-                sw.Stop();
             }
+
+            //int correct = 0;
+            //int mistakes = 1;
+
+            //while (true)
+            //{
+            //    notPrime:;
+            //    sw.Restart();
+            //    rng.GetBytes(bytes);
+            //    p = new BigInteger(bytes) - 1;
+            //    if (p < 3) goto notPrime;
+            //    foreach (var prime in primes)
+            //    {
+            //        if (p % prime == 0) goto notPrime;
+            //    }
+            //    if (BigInteger.ModPow(2, p - 1, p) == 1)
+            //    {
+            //        mistakes++;
+            //        goto notPrime;
+            //    }
+            //    correct++;
+
+            //    double errorRate = mistakes;
+            //    errorRate /= (double)(mistakes + correct);
+            //    errorRate = Math.Round(errorRate * 100, 5);
+            //    Console.WriteLine(correct.ToString("N0") + " correct guesses, " + mistakes.ToString("N0") + " mistakes. " + errorRate.ToString("N2") + "% error rate. " + primeByteSize.ToString("N0") + " bytesize.");
+            //    sw.Stop();
+            //}
+        }
+
+        static BigInteger CustomModExp(BigInteger a, BigInteger exponent, BigInteger modulus)
+        {
+            if (a < 2 || exponent < 2 || modulus < 2) throw new FormatException("an input is too small for CustomModExp()");
+            var b = new BigInteger(1);
+            while (true)
+            {
+                if (exponent % 2 == 1)
+                { 
+                    b *= a;
+                    b %= modulus;
+                }
+
+                a *= a;
+                a %= modulus;
+                exponent /= 2;
+                if (exponent <= 0) break;
+            }
+            return b;
         }
     }
 }
